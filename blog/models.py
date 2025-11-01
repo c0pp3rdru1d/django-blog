1441ba7c96d4d9e28dee2e64583fddac43ac79d2
c0pp3rdru1d <c0pp3rdru1d@proton.me>
2025-11-01 14:51:16 -0700

    Basic Layout Ready

---
+++ blog/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.utils.text import slugify
+
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.Slugfield(unique=True, blank=True, max_length=220)
+    excerpt = models.TextField()
+    cover = models.Imagefield(upload_to="covers/", blank=True, null=True)
+    published = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base = slugify(self.title)[:200]
+            slug = base
+            i = 1
+            while Post.objects.filter(slug=slug).exists():
+                i += 1
+                slug = f"{base}-{i}"
+            self.slug = slug
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
---
+++ tools/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./templates/**/*.html", "./blog/**/*.py"],
+  theme: { extend: {} },
+  plugins: [],
+}
+
+module.exports = { content: ["./templates/**/*.html", "./blog/**/*.py"], theme: { extend: {} }, plugins: [] }
